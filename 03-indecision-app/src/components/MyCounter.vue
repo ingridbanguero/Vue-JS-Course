80f09fa363556a3970344efbffa25b280be3105b
ingridbanguero <ingrid_banguero@hotmail.com>
2025-07-18 20:13:00 -0500

    Integracion de estilos con Tailwind CSS

---
+++ 03-indecision-app/src/components/MyCounter.vue
@@ -0,0 +1,47 @@
+<template>
+  <section>
+    <h3>Counter: {{ counter }}</h3>
+    <h3>Square: {{ squareCounter }}</h3>
+    <h4>{{ text }}</h4>
+    <div>
+      <button class="btn" @click="counter++">+1</button>
+      <button class="btn" @click="counter--">-1</button>
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import { computed, ref } from 'vue';
+
+/* Forma comun en JS
+const props = defineProps<Props>();
+  value: { type: Number, required: true },
+  text: { type: String, required: true },
+});
+*/
+
+interface Props {
+  value: number;
+  text: string;
+}
+
+const props = defineProps<Props>();
+
+const counter = ref(props.value);
+const text = ref(props.text);
+const squareCounter = computed(() => counter.value * counter.value);
+</script>
+
+<style scoped>
+.btn {
+  background-color: var(--color-blue-500);
+  padding: calc(var(--spacing) * 5);
+  border-radius: 0.25rem;
+  margin-right: calc(var(--spacing) * 2);
+  &:hover {
+    @media (hover: hover) {
+      background-color: var(--color-blue-700);
+    }
+  }
+}
+</style>
